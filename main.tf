006bd13adffe5ce43e18d99b1bd3a29c1825bfbe
Mohammedirshaq <82198906+Mohammedirshaq@users.noreply.github.com>
2025-05-19 18:46:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,29 @@ terraform {
 }
 provider "google" {
   project = "ferrous-plating-458312-q5"
-  region  = "us"  # Replace with your desired region
+  region  = "northamerica-south1"  # Replace with your desired region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         =  "vm0"
+  machine_type = "e2-micro"  # Replace with your desired machine type
+  zone         = "northamerica-south1-a"  # Replace with your desired zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"  # Replace with your desired image
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral external IP
+    }
+  }
+}
+
+output "instance_private_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].network_ip
 }
